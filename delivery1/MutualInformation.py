bc297d9ad08fc961302c31aa9cfdbe1b1868c1dd
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2022-04-04 21:14:32 +0300

    update mutualInformation

--- delivery1/MutualInformation.py
+++ delivery1/MutualInformation.py
@@ -1,8 +1,10 @@
 import math
 import operator
 
+from FileOperation import ReadCSV
 
-def mutual_information_probability(n_gram):
+
+def mutual_information_probability(frequencyDict):
     corpus = ReadCSV("corpus.json")
     mutual_info_dict = dict()
     total = 0
@@ -18,3 +20,4 @@ def mutual_information_probability(n_gram):
         mutual_info_dict[i] = math.log2(ngramFreq / value)
 
     return dict(sorted(mutual_info_dict.items(), key=operator.itemgetter(1), reverse=True))
+
