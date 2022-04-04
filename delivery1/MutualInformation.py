272c6e8af2b79cd85d6ef120d3ac4b7ae4b9d78b
yusuf talha erdem <48523790+yusuftalhaerdem@users.noreply.github.com>
2022-04-04 21:29:57 +0300

    minor update at mutual information
    lowly connected phareses will not be added to final dictionary

--- delivery1/MutualInformation.py
+++ delivery1/MutualInformation.py
@@ -8,16 +8,17 @@ def mutual_information_probability(frequencyDict):
     corpus = ReadCSV("corpus.json")
     mutual_info_dict = dict()
     total = 0
-    for i in corpus.keys():
-        total += corpus[i]
+    for i in corpus.values():
+        total += i
 
     for i in frequencyDict.keys():
         ngramFreq = frequencyDict[i] / total
-        value = 1
+        probability = 1
         for word in i.split():
-            value *= corpus[word] / total
-
-        mutual_info_dict[i] = math.log2(ngramFreq / value)
+            probability *= corpus[word] / total
+        
+        if probability>3:
+            mutual_info_dict[i] = math.log2(ngramFreq / value)
 
     return dict(sorted(mutual_info_dict.items(), key=operator.itemgetter(1), reverse=True))
 
