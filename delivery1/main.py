a21f2ca569f4ffbc3174e74f5b338f7d9ba02949
Sena Dilara Yangöz <66904315+senayngz@users.noreply.github.com>
2022-04-04 13:56:22 +0300

    more function calling statement added

--- delivery1/main.py
+++ delivery1/main.py
@@ -3,14 +3,35 @@ from Frequency import partOfSpeechFilter
 from tTest import tTest
 
 if __name__ == '__main__':
+    
+   #FREQUENCY
     bigrams_collocations_frequency = partOfSpeechFilter(FileOperation.ReadCSV("bigrams.json"), 2)
     FileOperation.writeToCSV(bigrams_collocations_frequency, "bigrams_collocations_frequency")
 
     trigrams_collocations_frequency = partOfSpeechFilter(FileOperation.ReadCSV("trigrams.json"), 3)
     FileOperation.writeToCSV(trigrams_collocations_frequency, "trigrams_collocations_frequency")
 
+    #TTEST
     bigrams_collocations_tTest = tTest(FileOperation.ReadCSV("bigrams.json"))
     FileOperation.writeToCSV(bigrams_collocations_tTest, "bigrams_collocations_tTest")
 
     trigrams_collocations_tTest = tTest(FileOperation.ReadCSV("trigrams.json"))
     FileOperation.writeToCSV(trigrams_collocations_tTest, "trigrams_collocations_tTest")
+
+    #MUTUALINFO
+    bigrams_collocations_mutual_info = tTest(FileOperation.ReadCSV("bigrams.json"))
+    FileOperation.writeToCSV(bigrams_collocations_mutual_info, "bigrams_collocations_mutual_info")
+
+    trigrams_collocations_mutual_info = tTest(FileOperation.ReadCSV("trigrams.json"))
+    FileOperation.writeToCSV(trigrams_collocations_mutual_info, "trigrams_collocations_mutual_info")
+
+    #FREQUENCY COMBINED WITH TTEST
+    bigrams_tTest = tTest(FileOperation.ReadCSV("bigrams.json"))
+
+    trigrams_tTest = tTest(FileOperation.ReadCSV("trigrams.json"))
+
+    bigrams_collocations_frequency_tTest = partOfSpeechFilter(bigrams_tTest, 2)
+    FileOperation.writeToCSV(bigrams_collocations_frequency_tTest, "bigrams_collocations_frequency_tTest")
+
+    trigrams_collocations_frequency_tTest = partOfSpeechFilter(trigrams_tTest, 3)
+    FileOperation.writeToCSV(trigrams_collocations_frequency_tTest, "trigrams_collocations_frequency_tTest")
